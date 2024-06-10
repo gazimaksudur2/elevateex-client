6dcaa28e1823a4c78085b7c50af2b8e40b040505
GaziMaksudur <gazimaksudur2@gmail.com>
2024-06-10 20:31:23 +0600

    basic structure for teacher dashboard is built

--- src/layout/TeacherDashboard/AddClass.jsx
+++ src/layout/TeacherDashboard/AddClass.jsx
@@ -1,9 +1,111 @@
-import React from 'react';
+import { useForm } from "react-hook-form";
 
 const AddClass = () => {
+    const immersive = "Let's open an immersive door to";
+    const career = "one's career.";
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
+
     return (
         <div>
-            AddClass
+            <header className="bg-white w-[92%] mx-auto">
+                <div className="container px-6 py-16 mx-auto">
+                    <div className="items-center lg:flex lg:flex-row-reverse lg:justify-evenly">
+                        <div className="w-full lg:w-1/2">
+                            <div className="lg:max-w-lg">
+                                <h1 className="text-3xl font-semibold text-gray-800 lg:text-4xl">Instructor Application <span className="text-blue-500">Form</span></h1>
+
+                                <p className="mt-3 text-gray-600 ">{immersive} <span className="font-medium text-blue-500">Build</span> {career}</p>
+                                <div className="flex flex-col mt-2 space-y-3 lg:space-y-0 lg:flex-row">
+                                    <form className="card-body" onSubmit={handleSubmit(onSubmit)}>
+                                        <div className="flex justify-center items-center gap-4">
+                                            <div className="form-control">
+                                                <label className="label">
+                                                    <span className="label-text">First Name</span>
+                                                </label>
+                                                <input type="text" placeholder="Your First Name" className="input input-bordered"  {...register("first_name")} required />
+                                            </div>
+                                            <div className="form-control">
+                                                <label className="label">
+                                                    <span className="label-text">Last Name</span>
+                                                </label>
+                                                <input type="text" {...register("last_name")} placeholder="Your Last Name" className="input input-bordered" required />
+                                            </div>
+                                        </div>
+                                        <div className="form-control">
+                                            <label className="label">
+                                                <span className="label-text text-sm">Email</span>
+                                            </label>
+                                            <input type="email" {...register("example")} placeholder="email" className="input input-bordered" required />
+                                        </div>
+                                        <div className="form-control">
+                                            <label className="label">
+                                                <span className="label-text text-sm">Profile Image</span>
+                                            </label>
+                                            <input type="file" {...register("example")} className="file-input file-input-bordered w-full" />
+                                        </div>
+                                        <div className="form-control">
+                                            <label className="label">
+                                                <span className="label-text text-sm">Your Course Category</span>
+                                            </label>
+                                            <select {...register("category")} className="select select-bordered w-full">
+                                                <option selected disabled>Select a Category</option>
+                                                <option>Web Development</option>
+                                                <option>Marketing</option>
+                                                <option>Business</option>
+                                                <option>Data Science</option>
+                                                <option>Design</option>
+                                                <option>Programming</option>
+                                            </select>
+                                            {/* <select defaultValue={"Category Name"} {...register('category', { required: true })}
+                                                className="select select-bordered w-full">
+                                                <option disabled value="default">Select a category</option>
+                                                <option value="salad">Salad</option>
+                                                <option value="pizza">Pizza</option>
+                                                <option value="soup">Soup</option>
+                                                <option value="dessert">Dessert</option>
+                                                <option value="drinks">Drinks</option>
+                                            </select> */}
+                                        </div>
+                                        <div className="form-control py-2">
+                                            <label className="label">
+                                                <span className="label-text text-sm">Your Expertise Level</span>
+                                            </label>
+                                            <div className="ml-2 flex justify-start items-center gap-5">
+                                                <div className="flex justify-center items-center gap-2">
+                                                    <input {...register("example")} type="radio" name="radio-2" className="radio radio-primary" />
+                                                    <h2 className="inline">beginner</h2>
+                                                </div>
+                                                <div className="flex justify-center items-center gap-2">
+                                                    <input {...register("example")} type="radio" name="radio-2" className="radio radio-primary" />
+                                                    <h2 className="inline">mid-level</h2>
+                                                </div>
+                                                <div className="flex justify-center items-center gap-2">
+                                                    <input {...register("example")} type="radio" name="radio-2" className="radio radio-primary" />
+                                                    <h2 className="inline">Experienced</h2>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="form-control mt-6 gap-3">
+                                            {/* <button className="btn btn-primary">Login</button> */}
+                                            <input type="submit" value={'Submit For Review'} className="w-full btn btn-primary" />
+                                            <input type="submit" value={'Request to Another'} className="w-full btn btn-primary" />
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col items-center justify-center w-full mt-6 lg:mt-0 lg:w-1/3">
+                            <img className="w-full h-full max-w-sm" src="https://img.freepik.com/free-vector/mathematics-concept-illustration_114360-3972.jpg?t=st=1717933152~exp=1717936752~hmac=9c1ba993cc23dda057b8f303938b8e5749f7867ba883a8b62799f4ee324c6dbd&w=740" alt="email illustration vector art" />
+                            <div className="space-y-2">
+                                <h2 className="text-2xl font-semibold font-roboto text-[#151515db]">Your <span className="text-blue-600">Journey</span> Begins Here</h2>
+                                <p className="text-[#151515bc]">Applying for our Community is a straightforward journey that starts with your dream to teach and motivate the mass learner to transform or shape a generation.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
         </div>
     );
 };
--- src/layout/UserDashboard/EnrolledClass.jsx
+++ src/layout/UserDashboard/EnrolledClass.jsx
@@ -3,7 +3,33 @@ import { Link } from "react-router-dom";
 const EnrolledClassName = ({ course }) => {
     return (
         <div>
-            <div className="max-w-2xl overflow-hidden bg-white rounded-lg shadow-md">
+            <section className="bg-gray-100 lg:py-12 lg:flex lg:justify-center rounded-lg">
+                <div
+                    className="overflow-hidden bg-white lg:mx-8 lg:flex lg:max-w-6xl lg:w-full lg:shadow-md lg:rounded-xl">
+                    <div className="lg:w-1/2">
+                        <div className="h-64 bg-cover lg:h-full bg-admin_class"></div>
+                    </div>
+
+                    <div className="max-w-xl px-6 py-12 lg:max-w-5xl lg:w-1/2">
+                        <h2 className="text-2xl font-semibold text-gray-800 md:text-3xl">
+                            Build Your New <span className="text-blue-500">Idea</span>
+                        </h2>
+
+                        <p className="mt-4 text-gray-500">
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                            Quidem modi reprehenderit vitae exercitationem aliquid dolores ullam temporibus enim expedita aperiam
+                            mollitia iure consectetur dicta tenetur, porro consequuntur saepe accusantium consequatur.
+                        </p>
+
+                        <div className="inline-flex w-full mt-6 sm:w-auto">
+                            <a href="#" className="inline-flex items-center justify-center w-full px-6 py-2 text-sm text-white duration-300 bg-gray-800 rounded-lg hover:bg-gray-700 focus:ring focus:ring-gray-300 focus:ring-opacity-80">
+                                Start Now
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            {/* <div className="max-w-2xl overflow-hidden bg-white rounded-lg shadow-md">
                 <img className="object-cover w-full h-64" src="https://images.unsplash.com/photo-1550439062-609e1531270e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="Article" />
 
                 <div className="p-6">
@@ -21,7 +47,7 @@ const EnrolledClassName = ({ course }) => {
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> */}
         </div>
     );
 };
