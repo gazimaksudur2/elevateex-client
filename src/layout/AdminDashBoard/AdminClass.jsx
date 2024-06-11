d9bbf43e8e5312e1779b901c87259701a380fa59
GaziMaksudur <gazimaksudur2@gmail.com>
2024-06-12 03:21:41 +0600

    Front design is 95% done almost

--- src/layout/AdminDashBoard/AdminClass.jsx
+++ src/layout/AdminDashBoard/AdminClass.jsx
@@ -4,17 +4,19 @@ const AdminClass = () => {
     return (
         <>
             <tr>
+                <td className="px-4 py-4 text-gray-800">
+                    <h2 className='font-medium'>Introduction to Programming</h2>
+                    <p className='text-xs'>Embark on a journey to learn Python basics and start coding with confidence in just 4 weeks.</p>
+                </td>
                 <td className="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
                     <div className="inline-flex items-center gap-x-3">
-                        <input type="checkbox" className="text-blue-500 border-gray-300 rounded" />
-
-                            <div className="flex items-center gap-x-2">
-                                <img className="object-cover w-10 h-10 rounded-full" src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt="" />
-                                    <div>
-                                        <h2 className="font-medium text-gray-800">Arthur Melo</h2>
-                                        <p className="text-sm font-normal text-gray-600">@authurmelo</p>
-                                    </div>
+                        <div className="flex items-center gap-x-2">
+                            <img className="object-cover w-10 h-10 rounded-full" src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt="" />
+                            <div>
+                                <h2 className="font-medium text-gray-800">Arthur Melo</h2>
+                                <p className="text-sm font-normal text-gray-600">@authurmelo</p>
                             </div>
+                        </div>
                     </div>
                 </td>
                 <td className="px-12 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
@@ -24,15 +26,18 @@ const AdminClass = () => {
                         <h2 className="text-sm font-normal text-emerald-500">Active</h2>
                     </div>
                 </td>
-                <td className="px-4 py-4 text-sm text-gray-500 whitespace-nowrap">Design Director</td>
-                <td className="px-4 py-4 text-sm text-gray-500 whitespace-nowrap">authurmelo@example.com</td>
-                <td className="px-4 py-4 text-sm whitespace-nowrap">
+                <td className='px-4 py-4 text-sm text-gray-500 whitespace-nowrap'>
+                    <button className='inline-flex items-center justify-center px-6 py-2 text-sm text-white duration-300 bg-gray-800 rounded-lg hover:bg-gray-700 focus:ring focus:ring-gray-300 focus:ring-opacity-80'>
+                        See Progress
+                    </button>
+                </td>
+                {/* <td className="px-4 py-4 text-sm whitespace-nowrap">
                     <div className="flex items-center gap-x-2">
                         <p className="px-3 py-1 text-xs text-indigo-500 rounded-full bg-indigo-100/60">Design</p>
                         <p className="px-3 py-1 text-xs text-blue-500 rounded-full bg-blue-100/60">Product</p>
                         <p className="px-3 py-1 text-xs text-pink-500 rounded-full bg-pink-100/60">Marketing</p>
                     </div>
-                </td>
+                </td> */}
                 <td className="px-4 py-4 text-sm whitespace-nowrap">
                     <div className="flex items-center gap-x-6">
                         <button className="text-gray-500 transition-colors duration-200 dark:hover:text-red-500 hover:text-red-500 focus:outline-none">
